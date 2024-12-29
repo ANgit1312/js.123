77b0525540be3bcb9d3130759d3940468d69f35c
ANgit1312 <125180309+ANgit1312@users.noreply.github.com>
2024-12-29 16:27:46 +0000

    conversion operations

---
+++ .devcontainer/01_basics/03_convop.js
@@ -0,0 +1,24 @@
+let score = "ankit"
+//const {score} = req.body
+
+//console.log(typeof score);
+
+// let  valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => nan
+// true => 1
+//null => 0
+
+let isLoggedIn = NaN
+// let booleanIsLoggedIN = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIN);
+// 1 => true; 0 => false ; null => false; undefined => false; NaN => false; 0.0 => false; "" => true, "ankit" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
