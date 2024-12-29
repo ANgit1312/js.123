dc3c0d414acfad1a7db0ed3616fa0bbffe6b0054
ANgit1312 <125180309+ANgit1312@users.noreply.github.com>
2024-12-29 15:58:50 +0000

    datatype

---
+++ .devcontainer/01_basics/02_dt.js
@@ -0,0 +1,19 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+let name
+let age
+let loggedIn 
+
+// number => 2 power 53
+// big int 
+// string => ""
+//boolean => true/false
+// null => standalone value
+//undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof "hitesh");
