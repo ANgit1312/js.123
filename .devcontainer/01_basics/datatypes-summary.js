1dfc57a8ca1daec7c21c3d7b170f937f8795188a
ANgit1312 <125180309+ANgit1312@users.noreply.github.com>
2024-12-31 17:09:43 +0000

    datatype summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,51 @@
+// 2 categorisation of data types
+// on the basis -
+//  how data is stored in memory
+//how data is accessed
+
+/********* PRIMITIVE *******///(CALL BY VALUE)
+// 7 TYPES
+// 1. STRING
+// 2. NUMBER
+// 3. BOOLEAN
+// 4. null
+// 5. undefined
+// 6. SYMBOL
+// 7. BIGINT
+
+const score = 90;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;// type undefined
+
+//symbol//
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);//false - because symbol is unique
+
+const bigNumber = 3456567892345789n
+
+console.log(typeof userEmail);
+
+
+
+/**** REFERENCE ****/
+// 3 TYPES
+// 1. ARRAY
+// 2. OBJECT
+// 3. FUNCTIONS
+
+const heros= ['Iron Man', 'Captain America', 'Thor']
+let myObj = {
+    name :"ankit",
+    age : 21,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof myFunction);//return of function is called function object
