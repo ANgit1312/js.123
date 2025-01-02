8c2963a612f6ef60df0286552f1da4fbad80414e
ANgit1312 <125180309+ANgit1312@users.noreply.github.com>
2025-01-02 16:50:40 +0000

    nuber and maths

---
+++ .devcontainer/01_basics/06_nummaths.js
@@ -0,0 +1,11 @@
+const score = 400
+console.log(score)
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.678456
+console.log(otherNumber.toPrecision(4
+));//careful with this one
